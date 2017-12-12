2cc6b472eb6140eee22db7dbdb5e256bb73ae466
76jimmordino <31480983+76jimmordino@users.noreply.github.com>
2017-12-12 09:01:54 -0600

    Add files via upload

--- ChargingDock.js
+++ ChargingDock.js
@@ -1,12 +1,20 @@
 //defines how all charging stations work.
 function ChargingDock(){
 //Instance variables
-    this.ports=; //finish from instructions
-    this.leds=[]; //finish from instructions
+    this.ports=[undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined]; //finish from instructions
+    this.leds=["red","red","red","red","red","red","red","red"]; //finish from instructions
 
 
 //Instance Fucntions
     this.plug = function(dvc){
+      for(let a=0; a<=this.leds.length; a++){
+        if(this.led[a]=="red"){
+          this.ports[a]=dvc;
+          if(this.juice<0.99)
+
+        }
+      )
+      }
 
         //type in here
     };
