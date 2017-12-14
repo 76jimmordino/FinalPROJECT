546e4d9ccb3f230b0c9045a1bd666d0233b3a250
76jimmordino <31480983+76jimmordino@users.noreply.github.com>
2017-12-14 09:21:38 -0600

    Add files via upload

--- ChargingDock.js
+++ ChargingDock.js
@@ -1,4 +1,6 @@
 //defines how all charging stations work.
+const Device = require("./Device.js");
+
 function ChargingDock(){
 //Instance variables
     this.ports=[undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined]; //finish from instructions
@@ -7,29 +9,17 @@ function ChargingDock(){
 
 //Instance Fucntions
     this.plug = function(dvc){
-      for(let a=0; a<=this.leds.length; a++){
-        if(this.leds[a]=="red"){
-          this.ports[a]=dvc;
-      }
-          if(dvc.juice<0.99){
-              this.leds= "yellow";
-          }
-          else if(dvc.juice >= 0.99){
-              this.leds = "green";
-          }
+      for(let s = 0; s<8; s++){
+        if(this.leds[s] == "red"){
+          this.port[s] = dvc;
+          this.leds[s] = "yellow";
         }
       }
-      return this.ports[a];
         //type in here
     };
 
     this.unplug = function(dvcIdx){
-        if(!(this.leds == "red" )){
-            let temp = this.ports[dvcIdx];
-            this.ports[dvcIdx] = undefined;
-            this.leds[dvcIdx]= "red";
-            return temp;
-        }
+        //type in here
     };
 
     this.chargeAll = function(min){
@@ -40,7 +30,30 @@ function ChargingDock(){
 }
 
 //defines the testing code
-function main(){}
+function main(){
+  let cd = new ChargingDock();
+
+      let d1 = new Device("phone",3000,10000);
+      let d2 = new Device("laptop",3000,15000);
+      let d3 = new Device("laptop",5000,15000);
+      let d4 = new Device("tablet",3000,15000);
+
+      d1.use(90);
+      d2.use(120);
+      d3.use(90);
+      d4.use(240);
+
+      cd.plug(d1);
+      cd.plug(d2);
+      cd.plug(d3);
+      cd.plug(d4);
+
+      cd.chargeAll(60);
+
+      cd.unplug(0);
+      cd.unplug(1);
+
+}
 
 //runs the main code
 main();
--- Device.js
+++ Device.js
@@ -1,4 +1,4 @@
-//Defines how all Devices with work.
+
 
 function Device(t,ma,c){
 
@@ -11,83 +11,97 @@ function Device(t,ma,c){
     this.rate = [0.0015,0.0235,0.23];
 
     //Instance Functions
-
-    this.power = function(){
-      return this.juice;
-    }
-    this.off = function(){
-      if(this.state == "on"){
-        this.state == "off";
-      }
-      else if(this.state == "idle"){
-        this.state == "off";
-      }
-
-    };
     this.on = function(){
         if(this.state == "off" && this.juice >0){
            this.state = "idle";
-        }
 
-        else if (this.state == "off" && this.juice<=0){
-            this.state = "off";
         }
-        else if (this.state == "idle" && this.juice>0){
-            this.state = "active";
-        }
-    };
-
-  this.sleep = function(){
-    if(this.state == "active"){
-      this.state = "idle";
     }
-  };
-
-  this.wake = function(){
-    if(this.state == "idle" && this.juice>0){
-      this.state = "active";
+    this.off = function(){
+        if(this.state == "idle" || this.juice<=0 || this.state == "active"){
+        this.state = "off";
     }
-    else if(this.state == "off" && this.juice >0){
-      this.state = "active";
+}
+    this.wake = function(){
+        if((this.state == "idle" || this.state == "off") && this.juice>0){
+        this.state = "active";
+      }
     }
-  };
+    this.sleep = function(){
+        if(this.state == "active" && this.juice>0){
+        this.state = "idle";
+    }
+}
 
     this.charge = function(min){
         //adds more electricity to the device's juice depending on its state
-          let charge = (this.millAmps / this.capacity);
-          let time = min / 60;
         if(this.state == "off"){
+            let charge = (this.millAmps / this.capacity);
             let output = 1 - this.rate[0];
+            let time = min / 60;
+            this.juice = this.juice + charge*output*time;
+
         }
-        else if(this.state == "idle"){
+        else if(this.state = "idle"){
+            let charge = (this.millAmps / this.capacity);
             let output = 1 - this.rate[1];
+            let time = min / 60;
+            this.juice = this.juice + charge*output*time;
         }
-        else if(this.state == "active"){
+        else if(this.state = "active"){
+            let charge = (this.millAmps / this.capacity);
             let output = 1 - this.rate[2];
+            let time = min / 60;
+            this.juice = this.juice + charge*output*time;
         }
-      this.juice = this.juice + charge*output*time;
         //resets juice to 1 if it has exceeded 1
         if(this.juice>1){
           this.juice=1;
-        }
-    };
-    this.use = function(min){
-      let time = min/60;
-      let input;
-      if(this.state == "off"){
-        input = this.rate[0];
       }
-      else if(this.state == "idle"){
-        input = this.rate[1];
-      }
-      else if(this.state == "active"){
-        input = this.rate[2];
-      }
-      this.juice = this.juice - time*input;
-    };
-}//end of the device declaration
+};
+
+  this.use = function(min){
+    let time = min/60
+
+    if(this.state == "off"){
+       this.juice = this.juice - this.rate[0];
+    }
+    else if(this.state == "idle"){
+       this.juice = this.juice - this.rate[1]
+    }
+    if(this.state == "active"){
+       this.juice = this.juice - this.rate[2]
+    }
+  }
+    this.power = function(){
+      return this.juice;
+    }
+
+}
+
+
+//end of the device declaration
 
 //defines the testing code.
-function main(){}
+function main(){
+  let flemphone = new Device("phone",3000,10000);
+  console.log(flemphone.power());
+  flemphone.on();
+  flemphone.wake();
+  flemphone.use(90);
+  console.log(flemphone.power());
+  flemphone.sleep();
+  flemphone.use(300);
+  console.log(flemphone.power());
+  flemphone.off();
+  flemphone.use(300);
+  console.log(flemphone.power());
+  flemphone.charge(60);
+  console.log(flemphone.power());
+  flemphone.charge(1000);
+  console.log(flemphone.power());
+
+}
 //runs the testing code.
 main();
+module.exports = Device;
